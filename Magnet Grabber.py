4e8fc9aa7eca1b332ccab2936fc13f3174d6bdea
DasHund007 <125661708+DasHund007@users.noreply.github.com>
2024-12-08 15:54:10 +0100

    Update Magnet Grabber.py

--- Magnet Grabber.py
+++ Magnet Grabber.py
@@ -1,37 +1,21 @@
-import pyperclip
-import time
+import pyperclip, time
 from colorama import Fore, init
 
 init(autoreset=True)
-
 output_file = 'magnet_links.txt'
 
 def check_clipboard():
-    previous_clipboard = ""
-    link_count = 0
-
-    with open(output_file, 'w') as f:
-        f.truncate(0)
-
+    previous_clipboard, link_count = "", 0
+    with open(output_file, 'w'): pass
     while True:
         try:
             current_clipboard = pyperclip.paste()
-            
             if current_clipboard.startswith('magnet:?') and current_clipboard != previous_clipboard:
-                with open(output_file, 'a') as f:
-                    f.write(current_clipboard + '\n')
-                
+                with open(output_file, 'a') as f: f.write(current_clipboard + '\n')
                 link_count += 1
-                print(Fore.GREEN + "Link saved:")
-                print(Fore.RED + current_clipboard)
-                print(Fore.BLUE + "Magnet link copied to clipboard.")
-                
-                if link_count % 2 == 0:
-                    print(Fore.YELLOW + "\n")
-
+                print(f"{Fore.GREEN}Link saved: {Fore.RED}{current_clipboard} {Fore.BLUE}x{link_count}")
             previous_clipboard = current_clipboard
             time.sleep(0.5)
-
         except KeyboardInterrupt:
             print(Fore.YELLOW + "\nClipboard monitoring stopped.")
             break
